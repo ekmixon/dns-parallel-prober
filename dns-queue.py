18eeb684b1f9023b8ba210b65037656e0d1b09a4
Sourcery AI <>
2022-08-02 18:58:55 +0000

    'Refactored by Sourcery'

--- dns-queue.py
+++ dns-queue.py
@@ -41,10 +41,14 @@ RANDOM_SUBDOMAINS = 5
 RANDOM_SUBDOMAINS_LENGTH = 6
 
 # Colours
-def prRed(skk): print("\033[91m {}\033[00m" .format(skk))
-def prGreen(skk): print("\033[92m {}\033[00m" .format(skk))
-def prCyan(skk): print("\033[96m {}\033[00m" .format(skk))
-def prYellow(skk): print("\033[93m {}\033[00m" .format(skk))
+def prRed(skk):
+    print(f"\033[91m {skk}\033[00m")
+def prGreen(skk):
+    print(f"\033[92m {skk}\033[00m")
+def prCyan(skk):
+    print(f"\033[96m {skk}\033[00m")
+def prYellow(skk):
+    print(f"\033[93m {skk}\033[00m")
 
 # valid domain names allow ASCII letters, digits and hyphen (and are case
 # insensitive)
@@ -80,17 +84,8 @@ class RealProber(threading.Thread):
         self.dns_server = dns_server
         self.dns_timeout = dns_timeout
         # used for storing the results
-        if results_collector is None:
-            # use the global object
-            self.res = res
-        else:
-            # used to save the output in a temporary object
-            self.res = results_collector
-
-        if error_collector is None:
-            self.err = err
-        else:
-            self.err = error_collector
+        self.res = res if results_collector is None else results_collector
+        self.err = err if error_collector is None else error_collector
 
     def run(self):
         resolver = dns.resolver.Resolver()
@@ -98,24 +93,25 @@ class RealProber(threading.Thread):
         # as per http://comments.gmane.org/gmane.comp.python.dnspython.user/144
         resolver.lifetime = self.dns_timeout
         try:
-            log.debug("{}: Resolving {} with nameserver {}".format(
-                self.name, self.target, self.dns_server))
+            log.debug(
+                f"{self.name}: Resolving {self.target} with nameserver {self.dns_server}"
+            )
+
             # it's a list
             resolver.nameservers = [self.dns_server, ]
             answer = resolver.query(self.target)
             for data in answer:
-                out = '{} | {}'.format(self.target, data)
+                out = f'{self.target} | {data}'
                 self.res.append(out)
-                # don't log to console, use file.
-                # log.info(out)
+                        # don't log to console, use file.
+                        # log.info(out)
         except dns.exception.Timeout as e:
             # we want to know if the DNS server is barfing
-            errmsg = "{}: {}".format(self.target, e)
+            errmsg = f"{self.target}: {e}"
             self.err.append(errmsg)
-            # log.warn(errmsg)
+                # log.warn(errmsg)
         except dns.exception.DNSException as e:
-            log.debug("Error in thread {} when querying {}: {}".format(
-                self.name, self.target, e))
+            log.debug(f"Error in thread {self.name} when querying {self.target}: {e}")
 
 
 class MockProber(RealProber):
@@ -126,12 +122,12 @@ class MockProber(RealProber):
     def run(self):
         # sleep for a small amount of time, between 0.1 and 0.9
         _sleep_for = abs(random.normalvariate(0.5, 0.5))
-        log.debug("Mock probe {} sleeping for {}...".format(self.name, _sleep_for))
+        log.debug(f"Mock probe {self.name} sleeping for {_sleep_for}...")
         time.sleep(_sleep_for)
         if random.random() > 0.7:
             log.debug("Mock probe {} found a result.")
-            res.append("{} | {}".format(self.target, '127.0.0.1'))
-        log.debug("Mock prober {} done".format(self.name))
+            res.append(f"{self.target} | 127.0.0.1")
+        log.debug(f"Mock prober {self.name} done")
 
 
 class LoggingThread(threading.Thread):
@@ -141,7 +137,7 @@ class LoggingThread(threading.Thread):
         self.log_event = log_event
         self.outfile = None
         if outfile is not None:
-            prGreen("[+] Saving results to {}...".format(outfile))
+            prGreen(f"[+] Saving results to {outfile}...")
             self.outfile = open(outfile, 'w')
         self.running = True
 
@@ -162,8 +158,8 @@ class LoggingThread(threading.Thread):
             self.log_event.wait()
             while len(res) > 0:
                 _el = res.popleft()
-                self.outfile.write('{}\n'.format(_el))
-                # print('{}'.format(_el))
+                self.outfile.write(f'{_el}\n')
+                        # print('{}'.format(_el))
             self.outfile.flush()
 
         self.outfile.close()
@@ -172,11 +168,8 @@ class LoggingThread(threading.Thread):
 def random_subdomain():
     """A generator that returns random subdomains, used for checking
     wildcard DNS"""
-    for i in range(RANDOM_SUBDOMAINS):
-        _random_subdomain = ''
-        for j in range(RANDOM_SUBDOMAINS_LENGTH):
-            _random_subdomain += random.choice(ALPHABET)
-        yield _random_subdomain
+    for _ in range(RANDOM_SUBDOMAINS):
+        yield ''.join(random.choice(ALPHABET) for _ in range(RANDOM_SUBDOMAINS_LENGTH))
 
 
 def subdomain_gen(max_subdomain_len):
@@ -188,10 +181,10 @@ def subdomain_gen(max_subdomain_len):
 
 def subdomain_len(max_subdomain_len):
     import math
-    total = 0
-    for i in range(max_subdomain_len):
-        total += math.factorial(len(ALPHABET)) / math.factorial(len(ALPHABET) - i - 1)
-    return total
+    return sum(
+        math.factorial(len(ALPHABET)) / math.factorial(len(ALPHABET) - i - 1)
+        for i in range(max_subdomain_len)
+    )
 
 
 def subdomain_fromlist(the_list):
@@ -200,7 +193,7 @@ def subdomain_fromlist(the_list):
     # disk access then we should be OK.
     """A generator that returns the content from a file without loading it all in memory"""
     with open(the_list) as f:
-        for line in f.readlines():
+        for line in f:
             yield line.replace('\n', '')
 
 
@@ -215,9 +208,9 @@ def subdomain_fromlist_len(the_list):
 # remaining will be left out. Reason: there's no replenishing of the queue when
 # doing wildcard dns checks.
 def fill(d, amount, dom, sub, nsvrs, dns_timeout, results_collector=None, error_collector=None):
-    for i in range(amount):
+    for _ in range(amount):
         # calls next() on the generator to get the next target
-        _target = '{}.{}'.format(next(sub), dom)
+        _target = f'{next(sub)}.{dom}'
         t = Prober(
             # pick a dns server
             random.choice(nsvrs),
@@ -250,7 +243,7 @@ def do_check_wildcard_dns(dom, nsvrs, dns_timeout):
             wildcard_results,
             wildcard_error)
         # wait for the probes to finish
-        for el in range(len(wildcard_checklist)):
+        for _ in range(len(wildcard_checklist)):
             t = wildcard_checklist.popleft()
             t.join()
     except KeyboardInterrupt as e:
@@ -265,10 +258,8 @@ def do_check_wildcard_dns(dom, nsvrs, dns_timeout):
     # for now we simply count the number of hits
     if len(wildcard_results) == RANDOM_SUBDOMAINS:
         raise SystemExit(
-            "{} random subdomains returned a hit; "
-            "It is likely this is a wildcard DNS server. "
-            "Use the -w option to skip this check.".format(
-                RANDOM_SUBDOMAINS))
+            f"{RANDOM_SUBDOMAINS} random subdomains returned a hit; It is likely this is a wildcard DNS server. Use the -w option to skip this check."
+        )
 
 
 def main(dom,
@@ -286,12 +277,13 @@ def main(dom,
     ###
     # output management
     #
-    print("[+] Output destination: '{}'".format(outfile))
+    print(f"[+] Output destination: '{outfile}'")
     if outfile is not None and os.path.exists(outfile):
         if overwrite is False:
             raise SystemExit(
-                "Specified file '{}' exists and overwrite "
-                "option (-f) not set".format(outfile))
+                f"Specified file '{outfile}' exists and overwrite option (-f) not set"
+            )
+
         else:
             prRed("[+] Output destination will be overwritten.")
     # print(
@@ -309,7 +301,7 @@ def main(dom,
     # finding DNS servers
     #
 
-    nsvrs = list()
+    nsvrs = []
     if use_nameserver:
         prCyan("[+] Using user-supplied name servers...")
         _nsvrs = use_nameserver
@@ -322,14 +314,14 @@ def main(dom,
         except KeyboardInterrupt as e:
             raise SystemExit(e)
     for ns in _nsvrs:
-        log.debug('ns: {}'.format(ns))
+        log.debug(f'ns: {ns}')
         try:
             nsvrs.append(socket.gethostbyname(str(ns)))
         except socket.gaierror as e:
-            log.error("[ ] Error when resolving {}: {}".format(ns, e))
-    if len(nsvrs) == 0:
+            log.error(f"[ ] Error when resolving {ns}: {e}")
+    if not nsvrs:
         raise RuntimeError("None of the supplied name servers resolve to a valid IP")
-    prCyan('[+] Using name servers: {}'.format(nsvrs))
+    prCyan(f'[+] Using name servers: {nsvrs}')
 
     #
     ###
@@ -356,13 +348,16 @@ def main(dom,
         # use the inbuilt subdomain generator
         sub = subdomain_gen(max_subdomain_len)
         total_domains = subdomain_len(max_subdomain_len)
-        prGreen("[+] Will search for subdomains made of all possible {}-characters permutations".format(max_subdomain_len))
+        prGreen(
+            f"[+] Will search for subdomains made of all possible {max_subdomain_len}-characters permutations"
+        )
+
     else:
         if not os.path.exists(infile):
-            raise SystemExit("{} not found".format(infile))
+            raise SystemExit(f"{infile} not found")
         sub = subdomain_fromlist(infile)
         total_domains = subdomain_fromlist_len(infile)
-        prGreen("[+] Will search for subdomains contained in '{}'".format(infile))
+        prGreen(f"[+] Will search for subdomains contained in '{infile}'")
 
     # trigger for logging; set every iteration loop, wait()ed for 
     # in the logging thread
@@ -372,7 +367,7 @@ def main(dom,
 
     # pre-loading of queue
     print("[+] DNS probing starting...")
-    log.debug("Progressbar initialised with {} max".format(total_domains))
+    log.debug(f"Progressbar initialised with {total_domains} max")
     # NOTE if python complains max_value is not found, you've installed
     # "progressbar" and not "progressbar2"
     bar = progressbar.ProgressBar(max_value=total_domains)
@@ -382,18 +377,15 @@ def main(dom,
         # ns = str(nsvrs[random.randint(0, len(nsvrs)-1)])[:-1]
         fill(d, max_running_threads, dom, sub, nsvrs, dns_timeout)
         running = True
-    except StopIteration:
+    except (StopIteration, KeyboardInterrupt):
         running = False
-    except KeyboardInterrupt:
-        running = False
-
     done = 0
     previous_len = len(d)
     while running:
         try:
             time.sleep(sleep_time)
             # go through the queue and remove the threads that are done
-            for el in range(len(d)):
+            for _ in range(len(d)):
                 _t = d.popleft()
                 if _t.is_alive():
                     # put it back in the queue until next iteration
@@ -433,7 +425,7 @@ def main(dom,
     prYellow("[+] Waiting for all threads to finish...")
     # waiting for all threads to finish, popping them one by one and join()
     # each...
-    for el in range(len(d)):
+    for _ in range(len(d)):
         t = d.popleft()
         t.join()
 
@@ -483,11 +475,9 @@ if __name__ == '__main__':
         "--max-subdomain-len",
         type=int,
         default=DEFAULT_MAX_SUBDOMAIN_LEN,
-        help=(
-            "Maximum length of the subdomain for bruteforcing. "
-            "Default: {}".format(DEFAULT_MAX_SUBDOMAIN_LEN)
-        )
+        help=f"Maximum length of the subdomain for bruteforcing. Default: {DEFAULT_MAX_SUBDOMAIN_LEN}",
     )
+
     parser.add_argument('-d', '--debug', action='store_true')
     parser.add_argument(
         '-n',
@@ -500,8 +490,9 @@ if __name__ == '__main__':
         '-t',
         '--dns-timeout',
         default=DEFAULT_DNS_TIMEOUT,
-        help="How long to wait for a DNS response. Default: {}s".format(DEFAULT_DNS_TIMEOUT)
+        help=f"How long to wait for a DNS response. Default: {DEFAULT_DNS_TIMEOUT}s",
     )
+
     parser.add_argument(
         '-w',
         '--no-check-wildcard-dns',
